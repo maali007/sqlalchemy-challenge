1dc4b71fda021a9680ee09284adec6d2e3b87f85
maali007 <maali007@umn.edu>
2021-03-20 23:56:30 -0500

    Some edits on app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Dependencies
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -7,19 +8,24 @@ import datetime as dt
 
 from flask import Flask, jsonify
 
+# Create engine
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflect database into new model
 Base = automap_base()
-# reflect the tables
+
+# Reflect tables
 Base.prepare(engine, reflect=True)
 
+# Save reference to the tables
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-
+# Flask setup
 app = Flask(__name__)
 
+
+# Flask routes
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -32,29 +38,30 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    session = Session(engine)
     mr_date = dt.date(2017, 8 ,23)
     last12 = mr_date - dt.timedelta(days=365)
-    session = Session(engine)
     pptn = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date.between(last12, mr_date)).all()
-    session.close()
     date_pptn = {date: prcp for date, prcp in pptn}
     return jsonify(date_pptn)
+    session.close()
 
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
     all_stations = session.query(Station.station).all()
-    session.close()
     return jsonify(all_stations)
+    session.close()
 
 @app.route("/api/v1.0/tobs")
 def tobs():
+    session = Session(engine)
     mr_date = dt.date(2017, 8 ,23)
     last12 = mr_date - dt.timedelta(days=365)
-    session = Session(engine)
     tempo = session.query(Measurement.tobs).filter(Measurement.station == "USC00519281").filter(Measurement.date.between(last12, mr_date)).all()
     session.close()
     return jsonify(tempo)
+    session = Session(engine)
 
 if __name__ == "__main__":
     app.run(debug=True)
